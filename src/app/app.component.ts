f67677ecc51e83bebabedf2eba381cbfee2713bb
eawebmaster20 <eaweb20@gmail.com>
2024-09-10 16:35:01 +0000

    feat: import other other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MainHeroComponent } from './components/main-hero/main-hero.component';
+import { MainContextComponent } from './components/main-context/main-context.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MainHeroComponent,
+    MainContextComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
