424949a83df8d402e40f0bd3512ecda2ec225669
eawebmaster20 <eaweb20@gmail.com>
2024-09-11 16:08:24 +0000

    feat: imort RouterLink

--- src/app/components/main-hero/main-hero.component.ts
+++ src/app/components/main-hero/main-hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-hero',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-hero.component.html',
   styleUrl: './main-hero.component.scss'
 })
