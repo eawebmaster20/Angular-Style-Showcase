8645a1435bd334290637ec6b37a7665e6ad95bf5
eawebmaster20 <eaweb20@gmail.com>
2024-09-11 16:07:55 +0000

    feat: add template styles and routerLink to component

--- src/app/components/main-context/main-context.component.ts
+++ src/app/components/main-context/main-context.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-context',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-context.component.html',
   styleUrl: './main-context.component.scss'
 })
