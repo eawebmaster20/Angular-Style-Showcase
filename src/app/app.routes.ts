36ba15a732fac208d86c4b676c684c7ad315d7a9
eawebmaster20 <eaweb20@gmail.com>
2024-09-11 16:06:39 +0000

    feat: create lazy loading routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./components/main-hero/main-hero.component').then(m => m.MainHeroComponent) },
+    { path: 'context', loadComponent: () => import('./components/main-context/main-context.component').then(m => m.MainContextComponent) },
+    { path: '**', redirectTo: 'home' }
+];
